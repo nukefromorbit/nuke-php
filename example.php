3ccaa1d51b7fe4899c3d0ca39b97dc1b2e7439c2
Alexandru Dobrescu <alexandru.aleee@gmail.com>
2024-08-23 16:16:56 +0300

    feat(nuke): service implementation

--- example.php
+++ example.php
@@ -11,7 +11,7 @@ $nukeSecret = 'f73c546f(..)6fa03bff';
 Nuke::setIdentifier($nukeIdentifier);
 Nuke::setSecret($nukeSecret);
 
-$time = time();
+// Request
 $headers = [
     'Nuke-Identifier' => 'da0119a2(...)75f45571',
     'Nuke-Signature' => 't=1724415508, v=9490a2023ccaced0803bf00392f6fef0f890037ce935cb2c8bfd30a8550c04b9',
@@ -26,26 +26,32 @@ $payload = [
 ];
 
 try {
+    // Validate request
     Nuke::verifyIdentifierAndSignature(
         $headers['Nuke-Identifier'],
         $headers['Nuke-Signature'],
         $payload
     );
-    echo 'Nuke Identifier and Signature verified with success.' . "\n";
-    echo "\n";
 
+    // Event construct
     $event = Event::construct($payload);
-    echo 'Event loaded with success.' . "\n";
-    echo "\n";
 
-    $payload['event']['data']['token'] = strrev($event->token);
+    // Response
+    $headers = [
+        'Nuke-Identifier' => 'da0119a2(...)75f45571',
+        'Nuke-Signature' => 't=1724415508, v=9490a2023ccaced0803bf00392f6fef0f890037ce935cb2c8bfd30a8550c04b9',
+    ];
+    $payload = [
+        'event' => [
+            'type' => 'verification',
+            'data' => [
+                'token' => strrev($event->token)
+            ],
+        ]
+    ];
 
-    echo 'Response:' . "\n";
-    echo '  Headers:' . "\n";
-    echo '    Nuke-Identifier: ' . Nuke::$identifier . "\n";
-    echo '    Nuke-Signature: ' . Nuke::getSignature(time(), $payload) . "\n";
-    echo "\n";
-    echo '  Payload: ' . json_encode($payload) . "\n";
+    echo 'Headers: ' . json_encode($headers) . "\n";
+    echo 'Payload: ' . json_encode($payload) . "\n";
 } catch (Exception $e) {
     echo get_class($e) . ': ' . $e->getMessage() . "\n";
 }
